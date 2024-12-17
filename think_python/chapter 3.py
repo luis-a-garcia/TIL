69b4375a4471b75a20d2a19e05409505e84b67f4
luisgarc <luisgarcia@usgs.gov>
2024-12-17 08:39:46 -0600

    adding python code to TIL

---
+++ think_python/chapter 3.py
@@ -0,0 +1,14 @@
+def print_lyrics():                               #this is a basic function, since it
+    print("I'm a lumberjack, and I'm ok." )       #has nothing in the parenthesis it 
+    print("I sleep all night and work all day.")  #takes no arguments
+
+print_lyrics
+
+print_lyrics()
+
+def print_twice(string):   #this is a function that takes a string as an
+    print(string)          #argument and prints it twice
+    print(string)
+
+print_twice("Dennis Moore, ")
+
---
+++ think_python/gen_python.py
@@ -0,0 +1,19 @@
+import math
+n = 17;
+r = 5.
+x
+n
+
+volume = (4/3)*math.pi*(r**3)
+
+volume
+
+x = 42
+
+math.cos(x)**2 + math.sin(x)**2
+
+math.e**2
+
+math.pow(math.e, 2)
+
+math.exp(2)
