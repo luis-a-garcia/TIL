e272548cf6bf148ee7b0a125075662b47dd2a388
luisgarc <luisgarcia@usgs.gov>
2024-12-19 09:12:05 -0600

    updated examples

--- think_python/chapter 3.py
+++ think_python/chapter 3.py
@@ -12,3 +12,39 @@ def print_twice(string):   #this is a function that takes a string as an
 
 print_twice("Dennis Moore, ")
 
+def repeat(word, n):
+    print(word * n)
+
+spam = 'Spam, '
+repeat(spam, 4)
+
+def first_two_lines():
+    repeat(spam, 4)
+    repeat(spam, 4)
+
+first_two_lines()
+
+def last_three_lines():
+    repeat(spam, 2)
+    print('(Lovely Spam, Wonderful Spam!)')
+    repeat(spam, 2)
+
+last_three_lines()
+
+def print_verse():
+    first_two_lines()
+    last_three_lines()
+
+print_verse()
+
+for i in range(2):
+    print("Verse", i)
+    print_verse()
+    print()
+
+def print_n_verses(n):
+    for i in range(n):
+        print_verse()
+        print()
+
+print_n_verses(4)
